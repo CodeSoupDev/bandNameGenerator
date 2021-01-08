65c392d408a688177ba80d76636e9ae54f878e6c
CodeSoup <craig.machin@gmail.com>
2021-01-08 22:35:12 +1100

    initial commit

---
+++ bandNameGenerator.py
@@ -0,0 +1,9 @@
+#code longform:
+#firstPart = input("What was the name of your first pet?\n")
+
+#secondPart = input("What was the name of the street you grew up on?\n")
+
+#Bandname = str(firstPart) + " " + str(secondPart)
+
+#Code shortened using input statements that resolve in a print statement
+print("Your band name could be: " + input("What was the name of your first pet?\n") + " " + input("What was the name of the street you grew up on?\n"))
